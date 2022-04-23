9b1fa6d9595b36b907ac0c0ccb9c5adaaa63f3d4
Lucas Neves <lucas54neves@gmail.com>
2022-04-23 19:06:06 -0300

    feat: update function that converts numeric month to string

--- src/utils/convert_numeric_month_to_string.py
+++ src/utils/convert_numeric_month_to_string.py
@@ -1,17 +1,20 @@
-def convert_numeric_month_to_string(numeric_month):
+from utils.translate_words import translate_words
+
+
+def convert_numeric_month_to_string(numeric_month, language):
     months = {
-        1: "January",
-        2: "February",
-        3: "March",
-        4: "April",
-        5: "May",
-        6: "June",
-        7: "July",
-        8: "August",
-        9: "September",
-        10: "October",
-        11: "November",
-        12: "December",
+        1: translate_words("january", language),
+        2: translate_words("february", language),
+        3: translate_words("march", language),
+        4: translate_words("april", language),
+        5: translate_words("may", language),
+        6: translate_words("june", language),
+        7: translate_words("july", language),
+        8: translate_words("august", language),
+        9: translate_words("september", language),
+        10: translate_words("october", language),
+        11: translate_words("november", language),
+        12: translate_words("december", language),
     }
 
     return months[numeric_month]
